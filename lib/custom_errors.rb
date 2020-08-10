5be51ee84dcbbd56ac587e6beabb037d52f6fcce
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-10 17:14:42 -0400

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,3 +1,5 @@
+
+
 class Person
   attr_accessor :partner, :name
 
@@ -6,8 +8,22 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    begin
+      if person.class == Person
+        self.partner = person
+        person.partner = self
+      else
+        raise PartnerError
+      end
+    rescue PartnerError => error
+      puts error.message
+    end
+  end
+
+  class PartnerError < StandardError
+    def message
+      "You must pass an instance of the class Person to the method get_married"
+    end
   end
 
 end
